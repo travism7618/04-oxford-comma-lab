73e771fa8506279ac50f18fb8d81921e2f6b99ad
Travis Matos <travism7618@hstat.org>
2018-11-08 00:49:52 +0000

    adds all code to past tests

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(arr)
-    
+    #if array has one just return 
+    if arr.length==1
+        arr.join
+    elsif arr.length==2
+         arr.join(" and ")
+    elsif arr.length>2 
+        arr.each_with_index.map do |word,index|
+            if index==(arr.length-1)
+                arr[index]=("and " << word)
+            end
+        end
+        arr.join(", ")
+    end
+    # if array has two add and 
+    #if array has more than one return value +comma if last item
 end
